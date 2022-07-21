cf7aeb3d3abb49cf9365afa1a309d53b17f7bec0
riley soloner <rileysoloner@gmail.com>
2022-07-21 10:34:50 -0400

    Done.

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import React from "react";
 function Login() {
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("I submit");
+    console.log("submit");
   }
 
   return (
